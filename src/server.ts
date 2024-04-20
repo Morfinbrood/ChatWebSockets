1c43c070b66f21ff71adc76d4d8aad7a3a92e80e
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-04-20 12:36:24 +0300

    Working commit - have some problems with messageData - truing to fix it and undesrtand what goin on

--- src/server.ts
+++ src/server.ts
@@ -15,31 +15,33 @@ wss.on("connection", (ws: WebSocket) => {
   let groupIds: string[];
   console.log(`connection WebSocket: ${ws}`);
 
-  ws.on(
-    "message",
-    (messageData: { type: string; groupIds: string[]; text: string }) => {
-      console.log(
-        `new message data: ${messageData}  groupIds: ${messageData.groupIds}`
-      );
-      console.log(`typeOf MessageData ${typeof messageData}`);
-      if (messageData.type === "join") {
-        groupIds = messageData.groupIds;
-        groupIds.forEach((groupId) => messageService.joinGroup(groupId, ws));
-      } else if (messageData.type === "message") {
-        if (groupIds) {
-          console.log(
-            `sending message to others clients with this groupIds: ${groupIds}`
-          );
-          const text = JSON.parse(messageData.text.toString());
-          groupIds.forEach((groupId) =>
-            messageService.sendMessage(groupId, text, ws)
-          );
-        }
-      } else {
-        console.log(`unknown message.type`);
+  ws.on("message", (messageData: any) => {
+    console.log(
+      `new messageData: ${messageData}  groupIds: ${JSON.stringify(
+        messageData.groupIds
+      )}`
+    );
+
+    console.log(`dublicate messageData: ${messageData}`);
+
+    console.log(`typeOf MessageData ${typeof messageData}`);
+    if (messageData.type === "join") {
+      groupIds = messageData.groupIds;
+      groupIds.forEach((groupId) => messageService.joinGroup(groupId, ws));
+    } else if (messageData.type === "message") {
+      if (groupIds) {
+        console.log(
+          `sending message to others clients with this groupIds: ${groupIds}`
+        );
+        const text = JSON.parse(messageData.text.toString());
+        groupIds.forEach((groupId) =>
+          messageService.sendMessage(groupId, text, ws)
+        );
       }
+    } else {
+      console.log(`unknown message.type`);
     }
-  );
+  });
 
   ws.on("close", () => {
     groupIds.forEach((groupId) => {
