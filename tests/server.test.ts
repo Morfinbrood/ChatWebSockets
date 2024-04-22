36746c54272b6e3e6a3adcf029e056d917605c03
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-04-22 08:30:50 +0300

    update tests

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,110 +1,70 @@
 import WebSocket from "ws";
-import request from "supertest";
 
-describe("Integration Tests", () => {
-  let serverUrl = "http://localhost:3000";
-  let ws1: WebSocket, ws2: WebSocket, ws3: WebSocket;
-  let ws1Messages: any[] = [];
-  let ws2Messages: any[] = [];
-  let ws3Messages: any[] = [];
-  let ws1Groups: string[] = [];
-  let ws2Groups: string[] = [];
-  let ws3Groups: string[] = [];
+describe("WebSocket Server Tests", () => {
+  let socket1: WebSocket;
+  let socket2: WebSocket;
 
-  let ws1UpgradeLog: string[] = [];
-  let ws2UpgradeLog: string[] = [];
-  let ws3UpgradeLog: string[] = [];
-
-  beforeAll((done) => {
-    ws1 = new WebSocket(`${serverUrl}/socket`);
-    ws2 = new WebSocket(`${serverUrl}/socket`);
-    ws3 = new WebSocket(`${serverUrl}/socket`);
-
-    ws1.on("message", (message: any) => {
-      ws1Messages.push(message);
-    });
-    ws2.on("message", (message: any) => {
-      ws2Messages.push(message);
-    });
-    ws3.on("message", (message: any) => {
-      ws3Messages.push(message);
-    });
-
-    ws1.on("join", (message: any) => {
-      ws1Groups.push(message);
-    });
-    ws2.on("join", (message: any) => {
-      ws2Groups.push(message);
-    });
-    ws3.on("join", (message: any) => {
-      ws3Groups.push(message);
-    });
-
-    ws1.on("upgrade", () => {
-      ws1UpgradeLog.push("upgrade");
-    });
-
-    ws2.on("upgrade", () => {
-      ws2UpgradeLog.push("upgrade");
-    });
-
-    ws3.on("upgrade", () => {
-      ws3UpgradeLog.push("upgrade");
-    });
-
-    const waitForConnection = () => {
-      if (ws1.readyState === WebSocket.OPEN && ws2.readyState === WebSocket.OPEN && ws3.readyState === WebSocket.OPEN) {
-        done();
-      } else {
-        setTimeout(waitForConnection, 100);
-      }
-    };
-
-    waitForConnection();
+  afterEach(() => {
+    // Close WebSocket connections after each test
+    if (socket1 && socket1.readyState === WebSocket.OPEN) {
+      socket1.close();
+    }
+    if (socket2 && socket2.readyState === WebSocket.OPEN) {
+      socket2.close();
+    }
   });
 
-  test("test 1: handleUpgrade event occurred", async () => {
-    expect(ws1UpgradeLog.length).toBe(1);
-    expect(ws1UpgradeLog[0]).toBe("upgrade");
-    expect(ws2UpgradeLog.length).toBe(1);
-    expect(ws2UpgradeLog[0]).toBe("upgrade");
-    expect(ws3UpgradeLog.length).toBe(1);
-    expect(ws3UpgradeLog[0]).toBe("upgrade");
+  it("should establish WebSocket connections", (done) => {
+    socket1 = new WebSocket("ws://localhost:3000/handleUpgrade");
+    socket2 = new WebSocket("ws://localhost:3000/handleUpgrade");
+
+    socket1.addEventListener("open", () => {
+      expect(socket1.readyState).toBe(WebSocket.OPEN);
+    });
+
+    socket2.addEventListener("open", () => {
+      expect(socket2.readyState).toBe(WebSocket.OPEN);
+      done();
+    });
   });
 
-  test("test 2: Client 1 sends message via HTTP request and waiting that no one ws triggered message ivent", async () => {
-    await request(serverUrl)
-      .post("/sendHTTPMessage")
-      .send({ type: "message", groupIds: ["group1"], text: "Message from client 1" })
-      .expect(200);
+  it("client 1 should receive regular message when client 2 sends regular message", (done) => {
+    socket1 = new WebSocket("ws://localhost:3000/handleUpgrade");
+    socket2 = new WebSocket("ws://localhost:3000/handleUpgrade");
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    socket1.addEventListener("open", () => {
+      const message = JSON.stringify({
+        type: "join",
+        groupId: "group1",
+        senderUUID: "client1Uuid",
+      });
+      socket1.send(message);
+    });
 
-    expect(ws1Messages.length).toBe(0);
-    expect(ws2Messages.length).toBe(0);
-    expect(ws3Messages.length).toBe(0);
-  });
+    socket2.addEventListener("open", () => {
+      const message = JSON.stringify({
+        type: "join",
+        groupId: "group1",
+        senderUUID: "client2Uuid",
+      });
+      socket2.send(message);
+    });
 
-  test("test 3: Client 1 and client 2 join group 1, then client 1 sends a test message", async () => {
-    ws1.send(JSON.stringify({ type: "join", groupId: "group1", senderUUID: "client1UUID" }));
+    socket1.addEventListener("message", (event: any) => {
+      const message = JSON.parse(event.data);
+      expect(message.type).toBe("message");
+      expect(message.text).toBe("test msg1");
+      done();
+    });
 
-    ws2.send(JSON.stringify({ type: "join", groupId: "group1", senderUUID: "client2UUID" }));
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    const testMessage = "test msg to groups 1";
-    ws1.send(JSON.stringify({ type: "message", groupIds: ["group1"], text: testMessage, senderUUID: "client1UUID" }));
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    expect(ws1Messages.length).toBe(0);
-    expect(ws2Messages.length).toBe(1);
-    expect(JSON.parse(ws2Messages[0]).text).toBe(testMessage);
-  });
-
-  afterAll(() => {
-    ws1.close();
-    ws2.close();
-    ws3.close();
+    setTimeout(() => {
+      const message = JSON.stringify({
+        type: "message",
+        groupIds: ["group1"],
+        senderUUID: "client2Uuid",
+        text: "test msg1",
+      });
+      socket2.send(message);
+    }, 500);
   });
 });
