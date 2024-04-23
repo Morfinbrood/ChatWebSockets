defd60c2fcd53ac309602cdf8f64b246a6613384
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-04-23 15:04:06 +0300

    refactor: change socket type from any to Duplex

--- src/middleware/upgradeMiddleware.ts
+++ src/middleware/upgradeMiddleware.ts
@@ -1,7 +1,8 @@
 import { Request } from "express";
+import { Duplex } from "node:stream";
 import WebSocket from "ws";
 
-export const upgradeMiddleware = (wss: WebSocket.Server) => (req: Request, socket: any, head: Buffer) => {
+export const upgradeMiddleware = (wss: WebSocket.Server) => (req: Request, socket: Duplex, head: Buffer) => {
   if (req.url === "/handleUpgrade" && req.method === "GET") {
     wss.handleUpgrade(req, socket, head, (connection) => {
       console.log("Connection upgraded");
